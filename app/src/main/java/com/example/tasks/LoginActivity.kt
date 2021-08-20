8094a3771e52590874ba04b5f54d9ee14203ae69
Vinicius Santos <viniciushacrinha1@gmail.com>
2021-08-20 03:46:45 -0300

    Creating the Login layout

---
+++ app/src/main/java/com/example/tasks/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+    override fun onClick(v: View) {
+        TODO("Not yet implemented")
+    }
+
+}
